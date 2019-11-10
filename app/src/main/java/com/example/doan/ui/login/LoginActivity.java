a64633dde56b370743e906ee85e0c5b5ac02c77e
Nhat Vinh <phannhatvinh10a@gmail.com>
2019-11-10 10:46:10 +0700

    Update giao diện màn hình login, sign up và màn hình danh sách

--- app/src/main/java/com/example/doan/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/doan/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -18,13 +19,13 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.doan.R;
-import com.example.doan.ui.login.LoginViewModel;
-import com.example.doan.ui.login.LoginViewModelFactory;
+import com.example.doan.signup;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -37,95 +38,50 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
+        final EditText usernameEditText = findViewById(R.id.usernameLogin);
+        final EditText passwordEditText = findViewById(R.id.passwordLogin);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final TextView signUpLink = findViewById(R.id.loginLink);
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
+        final Intent intent = new Intent(this, signup.class);
+        View.OnClickListener signUp = new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent);
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
-
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
             }
         };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        signUpLink.setOnClickListener(signUp);
 
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
+        //Code khi nhan vao nut Login
+        final Button login = findViewById(R.id.login);
+        View.OnClickListener loginBtn = new View.OnClickListener(){
+            public void onClick(View v)
+            {
+
             }
-        });
+        };
+        login.setOnClickListener(loginBtn);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+        //Code khi nhan vao nut login bang google
+        final ImageButton loginGoogle = findViewById(R.id.loginGoogle);
+        View.OnClickListener loginBtnGoogle = new View.OnClickListener(){
+            public void onClick(View v)
+            {
+
             }
-        });
+        };
+        loginGoogle.setOnClickListener(loginBtnGoogle);
+
+        //Code khi nhap vao nut login bang facebook
+        final ImageButton loginFacebook = findViewById(R.id.loginFacebook);
+        View.OnClickListener loginBtnFacebook = new View.OnClickListener(){
+            public void onClick(View v)
+            {
+
+            }
+        };
+        loginFacebook.setOnClickListener(loginBtnFacebook);
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-    }
-
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
-    }
 }
