ac824b879334799ce35e744e3ba364786d578da5
Phan Nhật Vinh <phannhatvinh10a@gmail.com>
2019-12-24 13:27:49 +0700

    Added settings and edit info layouts

--- app/src/main/java/com/example/doan/Settings_EditInfo.java
+++ app/src/main/java/com/example/doan/Settings_EditInfo.java
@@ -3,6 +3,8 @@ package com.example.doan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Settings_EditInfo extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Settings_EditInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings__edit_info);
+
+        ImageButton back = findViewById(R.id.settingsEditInfoBack);
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/doan/fragment_settings.java
+++ app/src/main/java/com/example/doan/fragment_settings.java
@@ -1,9 +1,11 @@
 package com.example.doan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,7 +13,18 @@ public class fragment_settings extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_navigation_notification, container, false);
+        View view =  inflater.inflate(R.layout.fragment_navigation_settings, container, false);
+
+        Button editInfo = view.findViewById(R.id.settingsEditInfoButton);
+        editInfo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(),Settings_EditInfo.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 
